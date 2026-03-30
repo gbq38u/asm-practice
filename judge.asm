ade2865322cfdf51cfc8a0773d139feaab71c32f
gbq38u <gbmailp@gmail.com>
2026-03-30 18:12:41 +0300

    french-cup

--- judge.asm
+++ judge.asm
@@ -1,7 +1,9 @@
 default rel
 section .data
-fmt_in  db "%ld %ld", 0
-fmt_out db "%ld", 10, 0
+fmt_in  db "%llu %llu", 0
+fmt_out db "%s", 10, 0
+yes_str db "YES", 0
+no_str  db "NO", 0
 section .bss
 a resq 1
 b resq 1
@@ -17,14 +19,19 @@ main:
     xor eax, eax
     call scanf
     mov rax, [a]
-    mov rbx, [b]
-    cqo
-    idiv rbx
+    add rax, [b]
+    jc overflow
     lea rdi, [fmt_out]
-    mov rsi, rdx
+    lea rsi, [no_str]
     xor eax, eax
     call printf
+    jmp finish
+overflow:
+    lea rdi, [fmt_out]
+    lea rsi, [yes_str]
+    xor eax, eax
+    call printf
+finish:
     xor eax, eax
     add rsp, 8
     ret
-    
