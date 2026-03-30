1368fd51335feb50b84cd274cae5435bac3a8e48
gbq38u <gbmailp@gmail.com>
2026-03-30 17:20:00 +0300

    division-with-est

--- judge.asm
+++ judge.asm
@@ -1,8 +1,7 @@
 default rel
 section .data
-fmt_in   db "%ld %ld", 0
-fmt_out  db "%ld", 10, 0
-msg_zero db "division by zero", 10, 0
+fmt_in  db "%ld %ld", 0
+fmt_out db "%ld", 10, 0
 section .bss
 a resq 1
 b resq 1
@@ -17,23 +16,15 @@ main:
     lea rdx, [b]
     xor eax, eax
     call scanf
-    mov rbx, [b]
-    cmp rbx, 0
-    je div_zero
     mov rax, [a]
+    mov rbx, [b]
     cqo
     idiv rbx
     lea rdi, [fmt_out]
-    mov rsi, rax
+    mov rsi, rdx
     xor eax, eax
     call printf
     xor eax, eax
     add rsp, 8
     ret
-div_zero:
-    lea rdi, [msg_zero]
-    xor eax, eax
-    call printf
-    xor eax, eax
-    add rsp, 8
-    ret
+    
